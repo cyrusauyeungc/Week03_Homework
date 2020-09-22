a72a7599654756ea3ecc4e7f0bc35b28c8a6dbc6
cyrusauyeungc <69232468+cyrusauyeungc@users.noreply.github.com>
2020-09-22 22:52:52 +0800

    Included comment in the 2 scripts
    Comments have been added to both main.py in PyBank and PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,19 +11,22 @@ csvpath = os.path.join('..','PyBank', 'Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader,None)
-       
-    total = 0
+    csv_header = next(csvreader,None) # skip the header
+    
+    # set all variable to 0
+    total = 0 # set all variable to 0
     row_count = 0
     maxx = 0
     minn = 0
-    for row in csvreader:
-        total += int(row[1])
-        row_count += 1
-        if float(row[1]) > float(maxx):
+    
+    # start reading through the csv
+    for row in csvreader: 
+        total += int(row[1])                    # add up all number in row[1]
+        row_count += 1                        # count the row number
+        if float(row[1]) > float(maxx):    # look for largest no. & date
             maxx = row[1]
             max_date = row[0]
-        if float(row[1]) < float(minn):
+        if float(row[1]) < float(minn):    # look for smallest no. & date
             minn = row[1]
             min_date = row[0]
     
@@ -35,6 +38,8 @@ with open(csvpath) as csvfile:
           f"\nGreatest Increase in Profits: {max_date} (${maxx})",
           f"\nGreatest Decrease in Profits: {min_date} (${minn})")
     
+    
+    # Print to text file inside folder 'analysis'
     txtpath = os.path.join('..','PyBank', 'analysis', 'Text Output.txt')
     file1 = open(txtpath,"w") 
     file1.writelines(f"Financial Analysis\n----------------------------\nTotal Months: {row_count}\nTotal: ${total}\nAverage  Change: ${round(total/row_count,2)}\nGreatest Increase in Profits: {max_date} (${maxx})\nGreatest Decrease in Profits: {min_date} (${minn})") 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,18 +11,18 @@ csvpath = os.path.join('..','PyPoll', 'Resources', 'election_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader,None)
+    csv_header = next(csvreader,None) # skip the header
     
-    row_count = 0
+    row_count = 0 # set variable to 0
     
-    cand={}
+    cand={}            # create a dictionary to hold the candidate and no.
     
     for row in csvreader:
-         row_count += 1
-         if row[2] not in cand:
-            cand[row[2]] = 0
-         cand[row[2]] = cand[row[2]] + 1
-
+         row_count += 1                                   # count row number
+         if row[2] not in cand:                          # Add candidate when row[2] is not found in dictionary
+            cand[row[2]] = 0                               # and assign 0 to the new candidate added
+         cand[row[2]] = cand[row[2]] + 1          # Add 1 to the counter for the corresponding candidate
+    
     print(f"Election Results",
           f"\n----------------------------",
           f"\nTotal Votes: {row_count}",
@@ -33,6 +33,8 @@ with open(csvpath) as csvfile:
             f"\nWinner: {max(cand, key=cand.get)}",
             f"\n----------------------------")
     
+    
+    # Export result to text file
     txtpath = os.path.join('..','PyPoll', 'analysis', 'Text Output.txt')
     file1 = open(txtpath,"w") 
     file1.writelines(f"Election Results\n----------------------------\nTotal Votes: {row_count}\n----------------------------")
@@ -40,10 +42,4 @@ with open(csvpath) as csvfile:
         file1.write(f"\n{row}: {format((cand[row]/row_count*100),'.3f')}% ({cand[row]})")
     file1.writelines(f"\n----------------------------\nWinner: {max(cand, key=cand.get)}\n----------------------------")    
     file1.close()
-
-
-# In[ ]:
-
-
-
 
